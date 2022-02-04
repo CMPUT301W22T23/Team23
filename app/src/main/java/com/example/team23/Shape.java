28a32270216cda5570ec71f3676aa58acadba4f3
Sivachow <57271468+Sivachow@users.noreply.github.com>
2022-02-04 15:46:22 -0700

    Update Shape.java

--- app/src/main/java/com/example/team23/Shape.java
+++ app/src/main/java/com/example/team23/Shape.java
@@ -3,5 +3,6 @@ package com.example.team23;
 public abstract class Shape {
     private int x;
     private int y;
-    public String color = "green";
+    public String color = "Yellow
+        ";
 }
